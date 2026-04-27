59df580e7f79aa4d88fa98adda8fcb76efd8122c
rosadidede044-ux <rosadidede044@gmail.com>
2026-04-27 10:09:27 +0700

    Update arcgis-hotspot.js

--- api/arcgis-hotspot.js
+++ api/arcgis-hotspot.js
@@ -1,8 +1,5 @@
 export default async function handler(req, res) {
   try {
-    // ambil parameter dari URL
-    const hours = parseInt(req.query.hours || "24");
-
     const url = "https://opsroom.sipongidata.my.id/api/opsroom/indoHotspot?wilayah=IN&filterperiode=false&satelit[]=NASA-MODIS&satelit[]=NASA-SNPP&satelit[]=NASA-NOAA20&confidence[]=low&confidence[]=medium&confidence[]=high";
 
     const response = await fetch(url);
@@ -10,25 +7,14 @@ export default async function handler(req, res) {
 
     const raw = json.data || [];
 
-    const now = new Date();
-
     const features = raw
       .filter(d => d.latitude && d.longitude)
-      .filter(d => {
-        // filter waktu (kalau ada field waktu)
-        if (!d.tanggal) return true;
-
-        const t = new Date(d.tanggal);
-        const diffHours = (now - t) / (1000 * 60 * 60);
-
-        return diffHours <= hours;
-      })
       .map((d) => ({
         type: "Feature",
         properties: {
           confidence: d.confidence,
           brightness: d.brightness,
-          tanggal: d.tanggal
+          waktu: d.tanggal || d.acq_date || d.datetime || "unknown"
         },
         geometry: {
           type: "Point",
